e9d672c1759a3988459354c087e0c1ac6e65217e
Oleg Yukhnevich <whyoleg@gmail.com>
2023-03-28 17:56:01 +0300

    support native marking as inaccessible

--- foreign-memory/src/jvmJdkFfmMain/kotlin/MemoryScope.kt
+++ foreign-memory/src/jvmJdkFfmMain/kotlin/MemoryScope.kt
@@ -4,24 +4,25 @@ import java.lang.foreign.*
 import java.lang.foreign.MemorySegment as JMemorySegment
 
 public actual abstract class MemoryScope {
-    protected abstract val scope: SegmentScope
-
     @ForeignMemoryApi
-    public actual fun allocateMemory(layout: MemoryLayout): MemorySegment {
-        return MemorySegment(JMemorySegment.allocateNative(layout.size, layout.alignment, scope))
+    protected fun SegmentScope.alloc(layout: MemoryLayout): MemorySegment {
+        return MemorySegment(JMemorySegment.allocateNative(layout.size, layout.alignment, this))
     }
 
-    public actual abstract class Closeable internal constructor(
-        private val arena: Arena
-    ) : MemoryScope() {
-        final override val scope: SegmentScope get() = arena.scope()
+    @ForeignMemoryApi
+    public actual abstract fun allocateMemory(layout: MemoryLayout): MemorySegment
+
+    public actual class Closeable internal constructor() : MemoryScope() {
+        //TODO: confined vs shared
+        private val arena = Arena.openShared()
+
+        @ForeignMemoryApi
+        override fun allocateMemory(layout: MemoryLayout): MemorySegment = arena.scope().alloc(layout)
         public actual fun close(): Unit = arena.close()
     }
 
     internal object Auto : MemoryScope() {
-        override val scope: SegmentScope get() = SegmentScope.auto()
+        @ForeignMemoryApi
+        override fun allocateMemory(layout: MemoryLayout): MemorySegment = SegmentScope.auto().alloc(layout)
     }
-
-    //TODO: confined vs shared
-    internal class Impl : Closeable(Arena.openShared())
 }
