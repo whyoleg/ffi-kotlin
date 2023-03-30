63e1b96442eab065bba44696482809460acbdbd4
Oleg Yukhnevich <whyoleg@gmail.com>
2023-03-30 19:51:16 +0300

    add some expects for number

--- foreign-platform/src/commonMain/kotlin/platform.kt
+++ foreign-platform/src/commonMain/kotlin/platform.kt
@@ -1,12 +1,12 @@
 package dev.whyoleg.foreign.platform
 
-public expect class PlatformInt {
+public expect class PlatformInt : Comparable<PlatformInt> {
     public companion object {
         public val SIZE_BYTES: Int
     }
 }
 
-public expect value class PlatformUInt internal constructor(internal val data: PlatformInt)
+public expect value class PlatformUInt internal constructor(internal val data: PlatformInt) : Comparable<PlatformUInt>
 
 public expect inline fun PlatformInt.toPlatformUInt(): PlatformUInt
 public expect inline fun PlatformUInt.toPlatformInt(): PlatformInt
