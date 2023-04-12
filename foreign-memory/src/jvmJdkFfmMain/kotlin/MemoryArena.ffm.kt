d8fb9579f05388a2293d1b5f507640892532ddbf
Oleg Yukhnevich <whyoleg@gmail.com>
2023-04-12 09:11:52 +0200

    fix compilation

--- foreign-memory/src/jvmJdkFfmMain/kotlin/MemoryArena.ffm.kt
+++ foreign-memory/src/jvmJdkFfmMain/kotlin/MemoryArena.ffm.kt
@@ -19,19 +19,40 @@ internal sealed class MemoryArenaImpl : MemoryArena {
 
     override fun wrap(address: MemoryAddress, layout: MemoryLayout): MemorySegment? = TODO()
 
+
     class Shared : MemoryArenaImpl() {
         private val arena = Arena.openShared()
-        private val cleaner = CLEANER.register(this, arena::close)
+        private val actions = mutableListOf<() -> Unit>()
+        private val cleaner = CLEANER.register(this, CleanupAction(arena, actions))
         override val scope: SegmentScope get() = arena.scope()
         override val allocator: MemoryAllocator get() = arena
         override fun close(): Unit = cleaner.clean()
+        override fun invokeOnClose(block: () -> Unit) {
+            actions.add(block)
+        }
+
+        private class CleanupAction(
+            private val arena: Arena,
+            private val actions: MutableList<() -> Unit>
+        ) : Runnable {
+            override fun run() {
+                actions.forEach { it.runCatching { invoke() } }
+                actions.clear()
+                arena.close()
+            }
+        }
     }
 
     object Implicit : MemoryArenaImpl() {
         override val scope: SegmentScope get() = SegmentScope.auto()
         override val allocator: MemoryAllocator get() = SegmentAllocator.nativeAllocator(scope)
+
         override fun close() {
-            //no-op
+            TODO("should not be called on implicit scope")
+        }
+
+        override fun invokeOnClose(block: () -> Unit) {
+            TODO("should not be called on implicit scope")
         }
     }
 }
