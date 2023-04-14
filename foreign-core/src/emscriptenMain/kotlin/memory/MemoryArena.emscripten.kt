b7b8654f3389d1392d6f5e9ed8a1ce3ff67c0e54
Oleg Yukhnevich <whyoleg@gmail.com>
2023-04-14 22:22:21 +0200

    merge modules

---
+++ foreign-core/src/emscriptenMain/kotlin/memory/MemoryArena.emscripten.kt
@@ -0,0 +1,69 @@
+package dev.whyoleg.foreign.memory
+
+import dev.whyoleg.foreign.internal.*
+
+@Suppress("ACTUAL_WITHOUT_EXPECT")
+@ForeignMemoryApi
+public actual typealias MemoryAllocator = WasmMemory
+
+@ForeignMemoryApi
+internal sealed class MemoryArenaImpl private constructor(
+    override val allocator: MemoryAllocator
+) : MemoryArena {
+    override fun allocate(layout: MemoryLayout): MemorySegment = TODO()
+    override fun allocateArray(elementLayout: MemoryLayout, elementsCount: Int): MemorySegment = TODO()
+    override fun allocateString(value: String): MemorySegment = TODO()
+
+    override fun wrap(address: MemoryAddress, layout: MemoryLayout): MemorySegment? = TODO()
+
+    class Shared(memory: WasmMemory) : MemoryArenaImpl(memory) {
+        private val actions = mutableListOf<() -> Unit>()
+        private val cleaner = createCleaner(FreeAction(actions))
+
+        override fun allocate(size: MemoryAddressSize, alignment: MemoryAddressSize): MemorySegment {
+            val address = allocator.malloc(size)
+            val segment = MemorySegment(address, size, allocator, null)
+
+            //TODO: will it work correctly?
+            val memory = allocator
+            actions.add {
+                segment.makeInaccessible()
+                memory.free(segment.address)
+            }
+            return segment
+        }
+
+        override fun close() = cleaner.clean()
+
+        override fun invokeOnClose(block: () -> Unit) {
+            actions.add(block)
+        }
+
+        private class FreeAction(private val actions: MutableList<() -> Unit>) : CleanupAction() {
+            override fun run() {
+                actions.forEach { it.runCatching { invoke() } }
+                actions.clear()
+            }
+        }
+    }
+
+    class Implicit(memory: WasmMemory) : MemoryArenaImpl(memory) {
+        override fun allocate(size: MemoryAddressSize, alignment: MemoryAddressSize): MemorySegment {
+            val address = allocator.malloc(size)
+            val cleaner = createCleaner(FreeAction(allocator, address))
+            return MemorySegment(address, size, allocator, cleaner)
+        }
+
+        override fun invokeOnClose(block: () -> Unit) {
+            TODO("should not be called on implicit scope")
+        }
+
+        override fun close() {
+            TODO("should not be called on implicit scope")
+        }
+
+        private class FreeAction(private val memory: WasmMemory, private val address: MemoryAddress) : CleanupAction() {
+            override fun run(): Unit = memory.free(address)
+        }
+    }
+}
---
+++ foreign-core/src/jvmMain/kotlin/library/LibraryLoader.kt
@@ -0,0 +1,33 @@
+package dev.whyoleg.foreign.library
+
+import java.util.*
+
+public interface LibraryLoader {
+    public val key: String
+    public val dependencies: List<String>
+    public fun load()
+}
+
+internal fun loadLibraries() {
+    val loaders = ServiceLoader.load(
+        LibraryLoader::class.java,
+        LibraryLoader::class.java.classLoader
+    ).iterator().asSequence().toList()
+
+    val loaded = mutableSetOf<String>()
+    val loadersByKeys = loaders.associateBy { it.key }
+    check(loaders.size == loadersByKeys.size) { "Duplicated library name" }
+
+    fun LibraryLoader.loadAll() {
+        dependencies.forEach { dependency ->
+            if (dependency in loaded) return@forEach
+            loadersByKeys.getValue(dependency).loadAll()
+        }
+        if (key in loaded) return
+        load()
+        loaded += key
+    }
+    loaders.forEach(LibraryLoader::loadAll)
+
+    check(loaders.size == loaded.size) { "Not all libraries are loaded" }
+}
