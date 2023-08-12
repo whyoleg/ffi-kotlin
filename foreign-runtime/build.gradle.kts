b571d4657500e1f0d5385015cd910e79d330a99e
Oleg Yukhnevich <whyoleg@gmail.com>
2023-08-12 22:59:55 +0300

    cleanup root

--- foreign-runtime/build.gradle.kts
+++ foreign-runtime/build.gradle.kts
@@ -1,28 +1,5 @@
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.*
-import org.jetbrains.kotlin.gradle.targets.js.yarn.*
-
 plugins {
-    id("build-parameters")
+    id("foreignbuild.conventions.multiplatform.root")
     alias(kotlinLibs.plugins.multiplatform) apply false
     alias(libs.plugins.android.library) apply false
-}
-
-plugins.withType<YarnPlugin> {
-    the<YarnRootExtension>().apply {
-        yarnLockMismatchReport = YarnLockMismatchReport.NONE
-        yarnLockAutoReplace = true
-    }
-}
-
-plugins.withType<NodeJsRootPlugin> {
-    the<NodeJsRootExtension>().apply {
-        nodeVersion = "20.0.0"
-    }
-}
-
-val skipTest = buildParameters.skip.test
-val skipLink = buildParameters.skip.link
-subprojects {
-    if (skipTest) tasks.matching { it.name.endsWith("test", ignoreCase = true) }.configureEach { onlyIf { false } }
-    if (skipLink) tasks.matching { it.name.startsWith("link", ignoreCase = true) }.configureEach { onlyIf { false } }
 }
---
+++ gradle/plugins/src/main/kotlin/foreignbuild/conventions/multiplatform/root.gradle.kts
@@ -0,0 +1,29 @@
+package foreignbuild.conventions.multiplatform
+
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.*
+import org.jetbrains.kotlin.gradle.targets.js.yarn.*
+
+plugins {
+    id("build-parameters")
+}
+
+plugins.withType<YarnPlugin> {
+    the<YarnRootExtension>().apply {
+        yarnLockMismatchReport = YarnLockMismatchReport.NONE
+        yarnLockAutoReplace = true
+    }
+}
+
+plugins.withType<NodeJsRootPlugin> {
+    the<NodeJsRootExtension>().apply {
+        nodeVersion = "20.0.0"
+    }
+}
+
+val skipTest = buildParameters.skip.test
+val skipLink = buildParameters.skip.link
+
+subprojects {
+    if (skipTest) tasks.matching { it.name.endsWith("test", ignoreCase = true) }.configureEach { onlyIf { false } }
+    if (skipLink) tasks.matching { it.name.startsWith("link", ignoreCase = true) }.configureEach { onlyIf { false } }
+}
