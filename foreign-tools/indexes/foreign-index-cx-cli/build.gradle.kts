46303f035db0095ca64a67a90c2bc7a03393b3ae
Oleg Yukhnevich <whyoleg@gmail.com>
2023-08-15 10:55:16 +0300

    download LLVM for all targets and link over libclang from it - need cleanup drop mingw for now

--- foreign-tools/indexes/foreign-index-cx-cli/build.gradle.kts
+++ foreign-tools/indexes/foreign-index-cx-cli/build.gradle.kts
@@ -1,11 +1,11 @@
 import de.undercouch.gradle.tasks.download.*
 import org.jetbrains.kotlin.gradle.plugin.mpp.*
 import org.jetbrains.kotlin.gradle.tasks.*
+import org.jetbrains.kotlin.konan.target.*
 import org.tukaani.xz.*
 
 plugins {
     id("foreignbuild.conventions.multiplatform.base")
-    id("foreignbuild.conventions.multiplatform.targets.native.desktop")
     alias(kotlinLibs.plugins.multiplatform)
     alias(libs.plugins.undercouch.download)
 }
@@ -41,7 +41,7 @@ val uncompressClangSources by tasks.registering {
     }
 }
 
-val unzipClangCHeaders by tasks.registering(Copy::class) {
+val unzipClangCHeaders by tasks.registering(Sync::class) {
     dependsOn(uncompressClangSources)
     from(tarTree(uncompressedFile)) {
         include("clang-11.1.0.src/include/clang-c/*.h")
@@ -53,16 +53,88 @@ val unzipClangCHeaders by tasks.registering(Copy::class) {
     into(layout.buildDirectory.dir("clang/include"))
 }
 
+val downloadMacosArm64LLVM by tasks.registering(Download::class) {
+    src("https://download.jetbrains.com/kotlin/native/apple-llvm-20200714-macos-aarch64-essentials.tar.gz")
+    dest(layout.buildDirectory.file("llvm/macos-arm64.tar.gz"))
+    overwrite(false)
+}
+
+val unzipMacosArm64LLVM by tasks.registering(Sync::class) {
+    dependsOn(downloadMacosArm64LLVM)
+    from(tarTree(resources.gzip(downloadMacosArm64LLVM.map { it.dest }))) {
+        include("apple-llvm-20200714-macos-aarch64-essentials/lib/*.dylib")
+        eachFile {
+            relativePath = RelativePath(true, name)
+        }
+        includeEmptyDirs = false
+    }
+    into(layout.buildDirectory.dir("llvm/lib/macos-arm64"))
+}
+
+val downloadMacosX64LLVM by tasks.registering(Download::class) {
+    src("https://download.jetbrains.com/kotlin/native/apple-llvm-20200714-macos-x64-essentials.tar.gz")
+    dest(layout.buildDirectory.file("llvm/macos-x64.tar.gz"))
+    overwrite(false)
+}
+
+val unzipMacosX64LLVM by tasks.registering(Sync::class) {
+    dependsOn(downloadMacosX64LLVM)
+    from(tarTree(resources.gzip(downloadMacosX64LLVM.map { it.dest }))) {
+        include("apple-llvm-20200714-macos-x64-essentials/lib/*.dylib")
+        eachFile {
+            relativePath = RelativePath(true, name)
+        }
+        includeEmptyDirs = false
+    }
+    into(layout.buildDirectory.dir("llvm/lib/macos-x64"))
+}
+
+val downloadLinuxX64LLVM by tasks.registering(Download::class) {
+    src("https://download.jetbrains.com/kotlin/native/llvm-11.1.0-linux-x64-essentials.tar.gz")
+    dest(layout.buildDirectory.file("llvm/linux-x64.tar.gz"))
+    overwrite(false)
+}
+
+val unzipLinuxX64LLVM by tasks.registering(Sync::class) {
+    dependsOn(downloadLinuxX64LLVM)
+    from(tarTree(resources.gzip(downloadLinuxX64LLVM.map { it.dest }))) {
+        include("llvm-11.1.0-linux-x64-essentials/lib/*.so.11.1")
+        eachFile {
+            relativePath = RelativePath(true, "libclang.so")
+        }
+        includeEmptyDirs = false
+    }
+    into(layout.buildDirectory.dir("llvm/lib/linux-x64"))
+}
+
 tasks.withType<CInteropProcess>().configureEach {
     dependsOn(unzipClangCHeaders)
 }
 
 kotlin {
+    // mingwX64 is not supported yet - needs clang built for mingw
+    macosArm64()
+    macosX64()
+    linuxX64()
+
     targets.withType(KotlinNativeTargetWithHostTests::class).configureEach {
         compilations.named("main") {
             cinterops.create("declarations") {
                 defFile("src/commonMain/interop/clang.def")
                 includeDirs(unzipClangCHeaders.map { it.destinationDir })
+            }
+        }
+
+        compilations.configureEach {
+            compilerOptions.configure {
+                val syncTask = when (konanTarget) {
+                    KonanTarget.MACOS_ARM64 -> unzipMacosArm64LLVM
+                    KonanTarget.MACOS_X64   -> unzipMacosX64LLVM
+                    KonanTarget.LINUX_X64   -> unzipLinuxX64LLVM
+                    else                    -> TODO("Not supported: $konanTarget")
+                }
+                freeCompilerArgs.add("-linker-option")
+                freeCompilerArgs.add(syncTask.map { "-L${it.destinationDir}" })
             }
         }
 
