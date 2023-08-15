6829439765c81dd9b65bbba9b7a0c092d8c20c9a
Oleg Yukhnevich <whyoleg@gmail.com>
2023-08-15 09:59:53 +0300

    download clang headers in build

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 dependencies {
     implementation("build:parameters")
     implementation(kotlinLibs.gradle.plugin)
-    implementation(libs.build.gradle.download)
+    implementation(libs.build.undercouch.download)
     implementation(libs.build.android)
 }
