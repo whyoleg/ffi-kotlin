f7bb39ab57a7e189b0c296a4d1ca0072c69481b4
Oleg Yukhnevich <whyoleg@gmail.com>
2023-08-25 23:45:20 +0300

    libs rollback

--- foreign-tools/settings.gradle.kts
+++ foreign-tools/settings.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            from(files("libs.versions.toml"))
+            from(files("../gradle/libs.versions.toml"))
         }
     }
 }
