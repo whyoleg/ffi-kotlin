c5bc50cd10054914475b76efd5ff5a0ba650de2e
Oleg Yukhnevich <whyoleg@gmail.com>
2023-08-26 00:43:16 +0300

    better dependency downloading

--- build-logic/src/main/kotlin/foreignbuild.setup-libclang.gradle.kts
+++ build-logic/src/main/kotlin/foreignbuild.setup-libclang.gradle.kts
@@ -1,7 +1,9 @@
 import foreignbuild.dependencies.*
 
-val setupClangLibsLinuxX64 by tasks.registering(SetupTarGzDependency::class) {
-    dependency("foreignbuild.kotlin-native:llvm-11.1.0-linux-x64-essentials@tar.gz")
+registerTarGzDependencySetupTask(
+    "setupClangLibsLinuxX64",
+    "foreignbuild.kotlin-native:llvm-11.1.0-linux-x64-essentials@tar.gz"
+) {
     include("llvm-11.1.0-linux-x64-essentials/lib/*.so.11.1")
     eachFile {
         relativePath = RelativePath(true, "libclang.so")
@@ -10,8 +12,10 @@ val setupClangLibsLinuxX64 by tasks.registering(SetupTarGzDependency::class) {
     into(temporaryDir)
 }
 
-val setupClangLibsMacosX64 by tasks.registering(SetupTarGzDependency::class) {
-    dependency("foreignbuild.kotlin-native:apple-llvm-20200714-macos-x64-essentials@tar.gz")
+registerTarGzDependencySetupTask(
+    "setupClangLibsMacosX64",
+    "foreignbuild.kotlin-native:apple-llvm-20200714-macos-x64-essentials@tar.gz"
+) {
     include("apple-llvm-20200714-macos-x64-essentials/lib/*.dylib")
     eachFile {
         relativePath = RelativePath(true, "libclang.dylib")
@@ -20,8 +24,10 @@ val setupClangLibsMacosX64 by tasks.registering(SetupTarGzDependency::class) {
     into(temporaryDir)
 }
 
-val setupClangLibsMacosArm64 by tasks.registering(SetupTarGzDependency::class) {
-    dependency("foreignbuild.kotlin-native:apple-llvm-20200714-macos-aarch64-essentials@tar.gz")
+registerTarGzDependencySetupTask(
+    "setupClangLibsMacosArm64",
+    "foreignbuild.kotlin-native:apple-llvm-20200714-macos-aarch64-essentials@tar.gz"
+) {
     include("apple-llvm-20200714-macos-aarch64-essentials/lib/*.dylib")
     eachFile {
         relativePath = RelativePath(true, "libclang.dylib")
@@ -30,8 +36,10 @@ val setupClangLibsMacosArm64 by tasks.registering(SetupTarGzDependency::class) {
     into(temporaryDir)
 }
 
-val setupClangHeaders by tasks.registering(SetupTarXzDependency::class) {
-    dependency("foreignbuild.llvm:clang:11.1.0@src.tar.xz")
+registerTarXzDependencySetupTask(
+    "setupClangHeaders",
+    "foreignbuild.llvm:clang:11.1.0@src.tar.xz"
+) {
     include("clang-11.1.0.src/include/clang-c/*.h")
     eachFile {
         relativePath = RelativePath(true, "clang-c", name)
