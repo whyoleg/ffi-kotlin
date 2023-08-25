17c7b85bcab18e9aad35091ee3968fb2b9682cb6
Oleg Yukhnevich <whyoleg@gmail.com>
2023-08-26 00:43:16 +0300

    better dependency downloading

---
+++ build-logic/src/main/kotlin/foreignbuild/dependencies/ArchiveDependencies.kt
@@ -0,0 +1,62 @@
+package foreignbuild.dependencies
+
+import org.gradle.api.*
+import org.gradle.api.file.*
+import org.gradle.api.internal.file.archive.compression.*
+import org.gradle.api.tasks.*
+import org.gradle.kotlin.dsl.*
+import org.tukaani.xz.*
+import java.io.*
+import java.net.*
+import javax.inject.*
+
+fun Project.registerZipDependencySetupTask(
+    name: String,
+    dependency: String,
+    action: Action<Sync>
+): TaskProvider<Sync> = registerArchiveDependencySetupTask(name, dependency, action) {
+    zipTree(it)
+}
+
+fun Project.registerTarGzDependencySetupTask(
+    name: String,
+    dependency: String,
+    action: Action<Sync>
+): TaskProvider<Sync> = registerArchiveDependencySetupTask(name, dependency, action) {
+    tarTree(gzip(it))
+}
+
+fun Project.registerTarXzDependencySetupTask(
+    name: String,
+    dependency: String,
+    action: Action<Sync>
+): TaskProvider<Sync> = registerArchiveDependencySetupTask(name, dependency, action) {
+    tarTree(XzArchiver(it))
+}
+
+private fun Project.registerArchiveDependencySetupTask(
+    name: String,
+    dependency: String,
+    action: Action<Sync>,
+    provide: ArchiveOperations.(File) -> Any
+): TaskProvider<Sync> {
+    val configuration = configurations.create("${name}Dependency")
+    dependencies.add(configuration.name, dependency)
+    val archiveOperations = objects.newInstance<Injected>().archiveOperations
+    return tasks.register<Sync>(name) {
+        from(provider { archiveOperations.provide(configuration.singleFile) })
+        action.execute(this)
+    }
+}
+
+private abstract class Injected @Inject constructor(
+    val archiveOperations: ArchiveOperations
+)
+
+private class XzArchiver(private val file: File) : CompressedReadableResource {
+    override fun read(): InputStream = XZInputStream(file.inputStream().buffered())
+    override fun getURI(): URI = URIBuilder(file.toURI()).schemePrefix("xz:").build()
+    override fun getBackingFile(): File = file
+    override fun getBaseName(): String = file.name
+    override fun getDisplayName(): String = file.path
+}
