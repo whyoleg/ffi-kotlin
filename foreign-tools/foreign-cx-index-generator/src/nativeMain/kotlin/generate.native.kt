2b299a6f6f1acba27dc174bd8682bc38c03f44c9
Oleg Yukhnevich <whyoleg@gmail.com>
2023-08-27 22:38:59 +0300

    cleanup generator a little

--- foreign-tools/foreign-cx-index-generator/src/nativeMain/kotlin/generate.native.kt
+++ foreign-tools/foreign-cx-index-generator/src/nativeMain/kotlin/generate.native.kt
@@ -5,29 +5,19 @@ import dev.whyoleg.foreign.cx.index.generator.internal.*
 import okio.*
 import okio.ByteString.Companion.encodeUtf8
 
-public actual fun CxIndex.Companion.generate(
-    headers: Set<String>,
-    includePaths: Set<String>
-): CxIndex {
-    val headersFile = createTemporaryHeadersFile(headers)
-    val compilerArgs = includePaths.map { "-I$it" }
+internal actual val SystemFileSystem: FileSystem get() = FileSystem.SYSTEM
 
+internal actual fun generateCxIndex(
+    headerFilePath: String,
+    compilerArgs: List<String>
+): CxIndex {
     val builder = CxIndexBuilder()
 
     useIndex { index ->
-        useTranslationUnit(index, headersFile, compilerArgs) { translationUnit ->
+        useTranslationUnit(index, headerFilePath, compilerArgs) { translationUnit ->
             CxIndexRunner(builder).run(index, translationUnit)
         }
     }
 
     return builder.build()
-}
-
-private fun createTemporaryHeadersFile(headers: Set<String>): String {
-    val fileInfo = headers.joinToString("\n") { "#include <$it>" }
-    val hashInfo = fileInfo.encodeUtf8().sha1().hex()
-    val headersPath = FileSystem.SYSTEM_TEMPORARY_DIRECTORY.resolve("$hashInfo.h")
-    val headersExists = FileSystem.SYSTEM.exists(headersPath)
-    if (!headersExists) FileSystem.SYSTEM.write(headersPath, mustCreate = true) { writeUtf8(fileInfo) }
-    return headersPath.toString()
 }
