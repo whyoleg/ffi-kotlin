92e829af71c9bc021995d1cc30370e4ad6e19241
Oleg Yukhnevich <whyoleg@gmail.com>
2023-08-29 12:22:31 +0300

    possibly support running on mingw cleanup setup-libclang

--- build-logic/src/main/kotlin/foreignbuild.setup-libclang.gradle.kts
+++ build-logic/src/main/kotlin/foreignbuild.setup-libclang.gradle.kts
@@ -4,30 +4,32 @@ registerTarGzDependencySetupTask(
     "setupClangLibsLinuxX64",
     "foreignbuild.kotlin-native:llvm-11.1.0-linux-x64-essentials@tar.gz"
 ) {
-    include("llvm-11.1.0-linux-x64-essentials/lib/*.so.11.1")
-    eachFile {
-        relativePath = RelativePath(true, "libclang.so")
-    }
+    include("llvm-11.1.0-linux-x64-essentials/lib/libclang.so.11.1")
+    eachFile { path = "libclang.so" }
+}
+
+registerZipDependencySetupTask(
+    "setupClangLibsMingwX64",
+    "foreignbuild.kotlin-native:llvm-11.1.0-windows-x64-essentials@zip"
+) {
+    include("llvm-11.1.0-windows-x64-essentials/lib/libclang.lib")
+    eachFile { path = "libclang.lib" }
 }
 
 registerTarGzDependencySetupTask(
     "setupClangLibsMacosX64",
     "foreignbuild.kotlin-native:apple-llvm-20200714-macos-x64-essentials@tar.gz"
 ) {
-    include("apple-llvm-20200714-macos-x64-essentials/lib/*.dylib")
-    eachFile {
-        relativePath = RelativePath(true, "libclang.dylib")
-    }
+    include("apple-llvm-20200714-macos-x64-essentials/lib/libclang.dylib")
+    eachFile { path = "libclang.dylib" }
 }
 
 registerTarGzDependencySetupTask(
     "setupClangLibsMacosArm64",
     "foreignbuild.kotlin-native:apple-llvm-20200714-macos-aarch64-essentials@tar.gz"
 ) {
-    include("apple-llvm-20200714-macos-aarch64-essentials/lib/*.dylib")
-    eachFile {
-        relativePath = RelativePath(true, "libclang.dylib")
-    }
+    include("apple-llvm-20200714-macos-aarch64-essentials/lib/libclang.dylib")
+    eachFile { path = "libclang.dylib" }
 }
 
 registerTarXzDependencySetupTask(
@@ -35,7 +37,5 @@ registerTarXzDependencySetupTask(
     "foreignbuild.llvm:clang:11.1.0@src.tar.xz"
 ) {
     include("clang-11.1.0.src/include/clang-c/*.h")
-    eachFile {
-        relativePath = RelativePath(true, "clang-c", name)
-    }
+    eachFile { path = "clang-c/$name" }
 }
