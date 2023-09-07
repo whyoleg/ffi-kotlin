ca8b0b3e24a4913b45b034cc163f5b6410f4bffd
Oleg Yukhnevich <whyoleg@gmail.com>
2023-09-07 17:30:20 +0300

    some partial bindings module declarations

---
+++ foreign-tools/foreign-cx-bindings/src/commonMain/kotlin/CxBindingPackage.kt
@@ -0,0 +1,28 @@
+package dev.whyoleg.foreign.cx.bindings
+
+import dev.whyoleg.foreign.cx.index.*
+import kotlinx.serialization.*
+import kotlin.jvm.*
+
+@Serializable
+@JvmInline
+public value class CxBindingPackageName(public val value: String)
+
+@Serializable
+public data class CxBindingPackageInfo(
+    val typedefs: Map<CxDeclarationName, CxDeclarationId>,
+    val records: Map<CxDeclarationName, CxDeclarationId>,
+    val enums: Map<CxDeclarationName, CxDeclarationId>,
+    val functions: Map<CxDeclarationName, CxDeclarationId>,
+)
+
+//int BN_abs_is_word(const BIGNUM *a, const BN_ULONG w);
+//int BN_is_zero(const BIGNUM *a);
+//int BN_is_one(const BIGNUM *a);
+//int BN_is_word(const BIGNUM *a, const BN_ULONG w);
+//int BN_is_odd(const BIGNUM *a);
+
+//int OPENSSL_buf2hexstr_ex(char *str, size_t str_n, size_t *strlength,
+//                          const unsigned char *buf, size_t buflen,
+//                          const char sep);
+//char *OPENSSL_buf2hexstr(const unsigned char *buf, long buflen);
