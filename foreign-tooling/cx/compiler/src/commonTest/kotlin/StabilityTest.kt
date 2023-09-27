a48da55b0e1cb4e12d9d7d2eab3032dc10123528
Oleg Yukhnevich <whyoleg@gmail.com>
2023-09-27 21:48:35 +0300

    update stability test

--- foreign-tooling/cx/compiler/src/commonTest/kotlin/StabilityTest.kt
+++ foreign-tooling/cx/compiler/src/commonTest/kotlin/StabilityTest.kt
@@ -8,40 +8,47 @@ import kotlin.time.*
 class StabilityTest {
     @Test
     fun test() {
+        val targets = listOf(
+            CxCompilerTarget.MacosArm64,
+            CxCompilerTarget.MacosX64,
+            CxCompilerTarget.MingwX64,
+            CxCompilerTarget.LinuxX64,
+            CxCompilerTarget.IosDeviceArm64,
+            CxCompilerTarget.IosSimulatorArm64,
+            CxCompilerTarget.IosSimulatorX64,
+        )
+        val headers = listOf(
+            "openssl/ec.h",
+            "openssl/bio.h",
+            "openssl/evp.h",
+            "openssl/bn.h",
+            "openssl/aes.h",
+            "openssl/types.h",
+            "openssl/encoder.h",
+            "openssl/ssl.h",
+        )
         val runs = buildList {
             repeat(100) {
-                listOf(
-                    CxCompilerTarget.MacosArm64,
-                    CxCompilerTarget.MacosX64,
-                    CxCompilerTarget.MingwX64,
-                    CxCompilerTarget.LinuxX64,
-                    CxCompilerTarget.IosDeviceArm64,
-                    CxCompilerTarget.IosSimulatorArm64,
-                    CxCompilerTarget.IosSimulatorX64,
-                ).forEach { target ->
-                    listOf(
-                        "openssl/ec.h",
-                        "openssl/bio.h",
-                        "openssl/evp.h",
-                        "openssl/bn.h",
-                        "openssl/aes.h",
-                        "openssl/types.h",
-                        "openssl/encoder.h",
-                        "openssl/ssl.h",
-                    ).forEach { header ->
+                targets.forEach { target ->
+                    headers.forEach { header ->
                         add(target to header)
                     }
                 }
             }
         }
 
+        val indexL = runs.size.toString().length
+        val targetL = targets.maxOf { it.toString().length }
+        val headerL = headers.maxOf { it.length }
+
         runs.shuffled().forEachIndexed { index, (target, header) ->
             val (result, time) = measureTimedValue {
                 CxCompiler.buildIndexOverOpenssl3(header, target)
             }
-            val r = runs.size.toString()
-            val i = index.toString().padStart(r.length)
-            println("[$i/$r] $target|$header: $time")
+            val indexP = (index + 1).toString().padStart(indexL)
+            val targetP = target.toString().padEnd(targetL)
+            val headerP = header.padEnd(headerL)
+            println("[$indexP/${runs.size}] $targetP | $headerP : $time")
             assertTrue(
                 result.enums.isNotEmpty() ||
                         result.records.isNotEmpty() ||
