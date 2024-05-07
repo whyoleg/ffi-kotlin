3d7707977c76bd6404da6bfd4de695be294ca527
Oleg Yukhnevich <whyoleg@gmail.com>
2024-05-08 01:11:21 +0300

    nodejs 22

--- foreign-kotlin/build.gradle.kts
+++ foreign-kotlin/build.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.*
 import org.jetbrains.kotlin.gradle.targets.js.npm.*
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.*
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
@@ -13,13 +12,5 @@ plugins.withType<NodeJsRootPlugin> {
         lockFileDirectory.set(layout.buildDirectory.dir("kotlin-js-store"))
         packageLockMismatchReport.set(LockFileMismatchReport.NONE)
         packageLockAutoReplace.set(true)
-    }
-    // for wasm support
-    extensions.configure<NodeJsRootExtension> {
-        version = "21.0.0-v8-canary202310177990572111"
-        downloadBaseUrl = "https://nodejs.org/download/v8-canary"
-    }
-    tasks.withType<KotlinNpmInstallTask>().configureEach {
-        args.add("--ignore-engines")
     }
 }
