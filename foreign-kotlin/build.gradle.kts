9226fab67b151474ad7969db4acbd9dfc9e59031
Oleg Yukhnevich <whyoleg@gmail.com>
2024-11-22 18:47:56 +0200

    Update Kotlin and kotlinx deps

--- foreign-kotlin/build.gradle.kts
+++ foreign-kotlin/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.targets.js.nodejs.*
 import org.jetbrains.kotlin.gradle.targets.js.npm.*
 
 plugins {
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.library) apply false
 }
