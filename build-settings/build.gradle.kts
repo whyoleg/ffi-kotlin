cc137084f1a5d915d0c6bd875365fdfaa44a84ee
Oleg Yukhnevich <whyoleg@gmail.com>
2025-03-30 16:09:38 +0300

    latest deps/gradle

--- build-settings/build.gradle.kts
+++ build-settings/build.gradle.kts
@@ -7,7 +7,7 @@ kotlin {
 }
 
 dependencies {
-    implementation("com.gradle:gradle-enterprise-gradle-plugin:3.17.2")
-    implementation("com.gradle:common-custom-user-data-gradle-plugin:2.0.1")
-    implementation("org.gradle.toolchains:foojay-resolver:0.8.0")
+    implementation("com.gradle:gradle-enterprise-gradle-plugin:3.19.2")
+    implementation("com.gradle:common-custom-user-data-gradle-plugin:2.2.1")
+    implementation("org.gradle.toolchains:foojay-resolver:0.9.0")
 }
